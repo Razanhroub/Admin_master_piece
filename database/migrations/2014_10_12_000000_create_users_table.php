7b8b4f381ad84636dfa2ab184ce698814b6b13a2
Razanhroub <razan.b.alhroub@gmail.com>
2025-08-25 23:51:30 +0300

    Fixing_migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,14 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->engine = 'InnoDB';
+            $table->string('last_name')->nullable();
+            $table->string('phone_number', 15)->nullable();
+            $table->text('address')->nullable();
+            $table->date('birth_of_date')->nullable();
+            $table->tinyInteger('is_deleted')->default(0);
+            $table->enum('role', ['admin', 'user'])->default('admin');
+            $table->string('profile_picture')->nullable();
         });
     }
 
